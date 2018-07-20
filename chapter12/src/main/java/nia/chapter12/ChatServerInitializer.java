7590f7f57f8dbf1e05f6eac4cd2685e5f1ce39d7
ByrsH <bgxjsh@sina.com>
2018-07-20 14:20:52 +0800

    WebSocket

---
+++ chapter12/src/main/java/nia/chapter12/ChatServerInitializer.java
@@ -0,0 +1,36 @@
+package nia.chapter12;
+
+import io.netty.channel.Channel;
+import io.netty.channel.ChannelInitializer;
+import io.netty.channel.ChannelPipeline;
+import io.netty.channel.group.ChannelGroup;
+import io.netty.handler.codec.http.HttpObjectAggregator;
+import io.netty.handler.codec.http.HttpServerCodec;
+import io.netty.handler.codec.http.websocketx.WebSocketServerProtocolHandler;
+import io.netty.handler.stream.ChunkedWriteHandler;
+
+/**
+ * @Author: yangrusheng
+ * @Description:
+ * @Date: Created in 9:56 2018/7/20
+ * @Modified By:
+ */
+public class ChatServerInitializer extends ChannelInitializer<Channel> {
+
+    private final ChannelGroup group;
+
+    public ChatServerInitializer(ChannelGroup group) {
+        this.group = group;
+    }
+
+    @Override
+    protected void initChannel(Channel channel) throws Exception {
+        ChannelPipeline pipeline = channel.pipeline();
+        pipeline.addLast(new HttpServerCodec());
+        pipeline.addLast(new ChunkedWriteHandler());
+        pipeline.addLast(new HttpObjectAggregator(64 * 1024));
+        pipeline.addLast(new HttpRequestHandler("/ws"));
+        pipeline.addLast(new WebSocketServerProtocolHandler("/ws"));
+        pipeline.addLast(new TextWebSocketFrameHandler(group));
+    }
+}
---
+++ chapter12/src/main/java/nia/chapter12/SecureChatServerInitializer.java
@@ -0,0 +1,32 @@
+package nia.chapter12;
+
+import io.netty.channel.Channel;
+import io.netty.channel.group.ChannelGroup;
+import io.netty.handler.ssl.SslContext;
+import io.netty.handler.ssl.SslHandler;
+
+import javax.net.ssl.SSLEngine;
+
+/**
+ * @Author: yangrusheng
+ * @Description:
+ * @Date: Created in 10:31 2018/7/20
+ * @Modified By:
+ */
+public class SecureChatServerInitializer extends ChatServerInitializer {
+
+    private final SslContext context;
+
+    public SecureChatServerInitializer(ChannelGroup group, SslContext context) {
+        super(group);
+        this.context = context;
+    }
+
+    @Override
+    protected void initChannel(Channel ch) throws Exception {
+        super.initChannel(ch);
+        SSLEngine engine = context.newEngine(ch.alloc());
+        engine.setUseClientMode(false);
+        ch.pipeline().addFirst(new SslHandler(engine));
+    }
+}
